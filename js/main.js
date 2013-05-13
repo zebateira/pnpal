f6c9360d8115d0c4ac7ec8a6a444ecbfa9f32a4d
Jose Bateira <zebateira@carsy-laptop.(none)>
2013-05-13 18:59:09 +0100

    Draws sprites from bmp.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+$(document).ready(function () {
+	var container = $('.sprite-container')
+	var max_rows = 70
+	var max_collumns = 63
 
+	function addSprite(row, collumn) {
+		container.append('<img class="sprite" data-row="' + row + '" data-collumn="' + collumn + '" />')
+	}
+
+	// add sprites to sprite container
+	for (var row = 0; row <= max_rows; row++) {
+		for (var collumn = 0; collumn <= max_collumns; collumn++) {
+			addSprite(row, collumn)
+		}
+		container.append('<br />')
+	}
+
+	// add positions of sprite image for every sprite
+	$('.sprite').each(function() {
+		$(this).css('background-position-x', calcSpritePosition($(this).attr('data-collumn')))
+			   .css('background-position-y', calcSpritePosition($(this).attr('data-row')))
+	})
+
+	function calcSpritePosition(coord) {
+		return - 32 * coord + "px"
+	}
+})
