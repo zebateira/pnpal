2440cc935d2649b6e1bbff86871d3a6a9404a54f
Zé Bateira <zebateira@carsy-laptop.com>
2013-05-19 02:08:25 +0100

    Active Navigation bar item Bug fix.

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
 $(document).ready(function($) {
 	// var navBarValues = ['home', 'pnp_player', 'map_builder', 'char_builder', 'about_project', 'about_team']
 
-	$('a.nav-item').click(function() {
-		//$('body > div.container').load('src/' + $(this).attr('id') + '.html')
-		$('li.active > a.nav-item').parent().removeClass('active')
-		$(this).parent().addClass('active')
-	})
-
 	window.onhashchange = function() {
 		var href = $('a[href^="' + document.location.hash + '"]').attr('href')
 		href = href.substr(href.indexOf('#') + 1)
 		$('body > div.container').load('src/' + href + '.html')
+
+		$('li.active > a.nav-item').parent().removeClass('active')
+		$('a.nav-item[href^="' + document.location.hash + '"]').parent().addClass('active')
 
 		console.log('link -> ' + href)
 	}
